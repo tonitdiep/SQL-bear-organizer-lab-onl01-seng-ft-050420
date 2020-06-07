150090362f0ae605c9b1b40225028b282651b52b
Toni  Diep <adiep123@gmail.com>
2020-06-07 00:50:32 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,8 @@
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 3, female, pink, happy, 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, "Rowdy", 3, female, pink, happy, 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Tabitha", 3, female, pink, happy, 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, "Sergeant Brown", 3, female, pink, happy, 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Melissa", 3, female, pink, happy, 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, "Mr. Chocolate", 3, female, pink, happy, 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "Mr. Chocolate", 3, female, pink, happy, 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, "Mr. Chocolate", 3, female, pink, happy, 0);
