84ccfa6fdaeeba1febcc27a74b2a57df65f7c436
Toni  Diep <adiep123@gmail.com>
2020-06-07 17:11:47 +0000

    Done.

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, "Mr. Chocolate", 3, "F", "pink", "happy", 0);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, "Rowdy", 3, "f", "pink", "mad", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Tabitha", 3, "F", "pink", "grumpy", 0);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, "Sergeant Brown", 3, "F", "pink", "angry", 1);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Melissa", 3, "F", "pink", "sad", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, "Tabitha", 44, "F", "pink", "grumpy", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, "Sergeant Brown", 9, "F", "pink", "angry", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, "Melissa", 1, "F", "pink", "sad", 0);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, "Grinch", 100, "F", "pink", "joyous", 1);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, "Wendy", 3, "F", "pink", "calm", 0);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, NULL, 3, "M", "pink", "hyper", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, NULL, 2, "M", "pink", "hyper", 1);
